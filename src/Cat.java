82bc414b94897741b2082d4a408d56818547f633
KatySid <whitekaty666@gmail.com>
2020-12-08 23:44:40 +0300

    Cat and Plate

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,36 @@
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean fullness;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean getFullness() {
+        return fullness;
+    }
+
+    public void eat(Plate plate) {
+        if (!fullness) {
+            fullness = plate.decreaseFood(appetite);
+        }
+        System.out.println(toString());
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cat " + name + ", appetite = " + appetite + ", fullness = " + fullness;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Cat[] cats = new Cat[5];
+        cats[0] = new Cat("Murzik", 5);
+        cats[1] = new Cat("Barsik", 15);
+        cats[2] = new Cat("Pushok", 50);
+        cats[3] = new Cat("Snegok", 25);
+        cats[4] = new Cat("Mars", 30);
+        Plate plate = new Plate(50);
+        plate.info();
+
+        // сначала просто по очереди кормим котов
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].eat(plate);
+            plate.info();
+        }
+        System.out.println("");
+        printInfoCats(cats);
+        System.out.println("");
+        //теперь  попробуем докормить голодных котов, добавляя в тарелку
+        while (!catsIsFullness(cats)) {
+            for (int i = 0; i < cats.length; i++) {
+                if(!cats[i].getFullness()){
+                    plate.addFood(10);
+                    plate.info();
+                    cats[i].eat(plate);
+                }
+            }
+        }
+        System.out.println("");
+        printInfoCats(cats);
+
+
+    }
+    public static void printInfoCats (Cat[] cats){
+        for (int i = 0; i < cats.length; i++) {
+            System.out.println(cats[i].toString());
+        }
+    }
+
+    public static boolean catsIsFullness(Cat[] cats) {
+        for (int i = 0; i < cats.length; i++) {
+            if (!cats[i].getFullness()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
